d348b7bf93362d375af9a37577317d6184feedd6
JunhOpportunity <twinjyjh5@gmail.com>
2022-05-07 21:00:10 +0900

    1 file modified

--- 7express/exp.js
+++ 7express/exp.js
@@ -12,5 +12,11 @@ app.get('/sky/:id', (req, res, next) => { // 만약 경로를 저렇게 작성�
   
 });
 
+app.post('/', (req, res, next) => { // 1 post 는 사용자가 무언가를 만들기 원하기 때문에 
+  app.use(express.json()); // 3 use 메소드를 사용해 json으로 바꿔야한다.
+  console.log(req.body); // 2 req 에서 body를 읽어와야 한다. 그런데 undefined로 표시되므로
+  
+})
+
 app.listen(8080);
 
